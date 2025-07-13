0128fc58f363f00207dd197b9d29f3e32eaa4922
Samuel Elias <samuel.elias@ibm.com>
2025-07-13 14:15:44 -0300

    start serve da api

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+
+const app = express()
+
+// GET
+// PUT / PATCH
+// DELETE
+// POST
+use.app()
+
+app.get('/', (req , res) => {
+
+})
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Meu servidor está rodando em https://localhost:3000`)
+})
